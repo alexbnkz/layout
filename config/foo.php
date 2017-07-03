fa68e0a794788185b2b864d3b725732d7393e8f1
Alex Benincasa <alex.cultura.rj@gmail.com>
2017-07-03 10:02:25 -0300

    footer example

---
+++ config/foo.php
@@ -0,0 +1,13 @@
+<footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col l4 offset-l2 s12 white-text">
+        footer do site
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+    © 2014 Copyright Text
+  </div>
+</footer>
