bc815a33feeb7ddef3501bffdeeabff3a07f3f27
Alex Benincasa <alex.cultura.rj@gmail.com>
2017-07-03 10:05:21 -0300

    artigos e contato

---
+++ artigos.php
@@ -0,0 +1,17 @@
+<?php include 'config/app.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Artigos</title>
+<?php echo inc('config.head'); ?>
+</head>
+<body>
+<?php echo inc('config.menu'); ?>
+
+<div>só artigos</div>
+
+<?php echo inc('config.foo'); ?>
+</body>
+</html>
+
---
+++ contato.php
@@ -0,0 +1,18 @@
+<?php include 'config/app.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Contato</title>
+<?php echo inc('config.head'); ?>
+</head>
+<body>
+<?php echo inc('config.menu'); ?>
+
+
+<div>mande seu e-mail para <a href="mailto:contato@tjpassos.com?subject=feedback">contato@tjpassos.com</a></div>
+
+<?php echo inc('config.foo'); ?>
+</body>
+</html>
+
