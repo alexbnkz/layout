9924f191bfee8d620bb6c88eadee24286adf91fd
Alex Benincasa <alex.cultura.rj@gmail.com>
2017-07-03 10:02:04 -0300

    menu nav

---
+++ config/menu.php
@@ -0,0 +1,9 @@
+
+<nav>
+  <ul id="main">
+			<li><a href="home.php">Home</a></li>
+			<li><a href="artigos.php">Artigos</a></li>
+			<li><a href="contato.php">Contato</a></li>
+    <div id="marker"></div>
+  </ul>
+</nav>
