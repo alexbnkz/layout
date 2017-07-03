8e8fe3b839ce29ab67e0c3eefbc4a17db1127130
Alex Benincasa <alex.cultura.rj@gmail.com>
2017-07-03 09:09:14 -0300

    inc só inclui php files

--- config/app.php
+++ config/app.php
@@ -1,10 +1,12 @@
 <?php
 
 function inc($filename) {
-    if (is_file($filename)) {
+    if (is_file($filename.'.php')) {
         ob_start();
         include $filename;
         return ob_get_clean();
     }
     return false;
 }
+
+// colocar configurações do banco
