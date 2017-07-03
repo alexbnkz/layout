8c330c44e8c760cacea837d192032598e1cff80a
Alex Benincasa <alex.cultura.rj@gmail.com>
2017-07-03 10:01:45 -0300

    home page

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php include 'config/app.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+<?php echo inc('config.head'); ?>
+</head>
+<body>
+<?php echo inc('config.menu'); ?>
+
+<div>texto meu, texto meu</div>
+
+<?php echo inc('config.foo'); ?>
+</body>
+</html>
+
